8b28dd7d16029a5d68d4a4f0cf38b86ea4cd3162
venkii83 <51102702+venkii83@users.noreply.github.com>
2020-12-28 00:58:34 -0500

    Add files via upload

---
+++ tuning_3.sql
@@ -0,0 +1,19 @@
+USE tuning;
+
+-- -------------------------------------
+SET @v1 = 1612521;
+SET @v2 = 1145072;
+SET @v3 = 1828467;
+SET @v4 = 'MGT382';
+SET @v5 = 'Amber Hill';
+SET @v6 = 'MGT';
+SET @v7 = 'EE';			  
+SET @v8 = 'MAT';
+
+-- 3. List the names of students who have taken course v4 (crsCode).
+
+SELECT name 
+FROM Student
+INNER JOIN Transcript
+ON Transcript.StudId = Student.Id
+WHERE Transcript.CrsCode = @v4;
